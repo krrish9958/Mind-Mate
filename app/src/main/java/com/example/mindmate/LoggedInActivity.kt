0a196c4448c0c7716840d80bfd8f6af24d3768ff
Deeksha Agarwal <agarwaldeeksha03@gmail.com>
2023-04-18 23:15:27 +0530

    Made some UI changes and fixed some bugs

--- app/src/main/java/com/example/mindmate/LoggedInActivity.kt
+++ app/src/main/java/com/example/mindmate/LoggedInActivity.kt
@@ -11,20 +11,10 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class LoggedInActivity : AppCompatActivity() {
-    private lateinit var continueBtn : Button
-    private lateinit var logoutBtn : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_logged_in)
-        continueBtn = findViewById(R.id.continueBtn)
-        continueBtn.setOnClickListener {
-            startActivity(Intent(this@LoggedInActivity,FeelingsActivity::class.java))
-        }
 
-        logoutBtn = findViewById(R.id.logoutBtn)
-        logoutBtn.setOnClickListener {
-            Firebase.auth.signOut()
-            startActivity(Intent(this@LoggedInActivity, LoggedOutActivity::class.java))
-        }
     }
 }
