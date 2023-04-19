438368fdf1b877e29914de9b901644c23ee9b691
Deeksha Agarwal <agarwaldeeksha03@gmail.com>
2023-04-19 20:50:30 +0530

    added mood tracker, it still have some issues

--- app/src/main/java/com/example/mindmate/FeelingsActivity.kt
+++ app/src/main/java/com/example/mindmate/FeelingsActivity.kt
@@ -1,26 +1,165 @@
 package com.example.mindmate
 
 import android.content.Intent
+import android.os.Build.VERSION_CODES.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import com.example.mindmate.utilities.*
 import com.google.firebase.auth.FirebaseAuth
 
 class FeelingsActivity : AppCompatActivity() {
-    private lateinit var continueButton : Button
     private lateinit var auth: FirebaseAuth
     private lateinit var username : TextView
+    private lateinit var etMood : EditText
+    private lateinit var tvEmoji : TextView
+    private lateinit var saveBtn : Button
+    private lateinit var continueButton : Button
+    private lateinit var happyKeywords : ArrayList<String>
+    private lateinit var sadKeywords : ArrayList<String>
+    private lateinit var angryKeywords : ArrayList<String>
+    private lateinit var depressedKeywords : ArrayList<String>
+    private lateinit var anxiousKeywords : ArrayList<String>
+    private lateinit var scaredKeywords : ArrayList<String>
+    private lateinit var tiredKeywords : ArrayList<String>
+    private lateinit var cryingKeywords : ArrayList<String>
+    private lateinit var mehKeywords : ArrayList<String>
+    private lateinit var relievedKeywords : ArrayList<String>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_feelings)
-        auth = FirebaseAuth.getInstance()
-        val name = intent.getStringExtra("name")
-        username = findViewById(R.id.username)
-        username.text = "Hey " + name
-        continueButton = findViewById(R.id.continueButton)
+        username = findViewById(R.id.usernameFeelings)
+        etMood = findViewById(R.id.etMood)
+        continueButton = findViewById(R.id.continueBtn)
+        tvEmoji = findViewById(R.id.tvEmoji)
         continueButton.setOnClickListener {
             startActivity(Intent(this@FeelingsActivity, HomeActivity::class.java))
         }
+
+        keywordsDefinition()
+        detectMood()
+
+    }
+
+    private fun detectMood() {
+        val userMood = etMood.text.toString().toLowerCase().trim()
+
+        var detectedMood : String? = null
+
+        if (happyKeywords.any{ userMood.contains(it) }) {
+            detectedMood = "happy"
+        }
+        else if (sadKeywords.any{ userMood.contains(it)}){
+            detectedMood = "sad"
+        }
+        else if (angryKeywords.any{ userMood.contains(it)}){
+            detectedMood = "angry"
+        }
+        else if (depressedKeywords.any { userMood.contains(it) }){
+            detectedMood = "depressed"
+        }
+        else if(anxiousKeywords.any { userMood.contains(it) }){
+            detectedMood = "anxious"
+        }
+        else if (scaredKeywords.any{ userMood.contains(it)}){
+            detectedMood = "scared"
+        }
+        else if (tiredKeywords.any{ userMood.contains(it)}){
+            detectedMood = "tired"
+        }
+        else if (cryingKeywords.any{ userMood.contains(it)}){
+            detectedMood = "crying"
+        }
+        else if (mehKeywords.any{ userMood.contains(it)}){
+            detectedMood = "meh"
+        }
+        else if (relievedKeywords.any{ userMood.contains(it)}){
+            detectedMood = "relieved"
+        }
+
+        when (detectedMood){
+            "happy" -> {
+                tvEmoji.text = HAPPY_EMOJI
+            }
+            "sad" -> {
+                tvEmoji.text = SAD_EMOJI
+            }
+            "depressed" -> {
+                tvEmoji.text = DEPRESSED_EMOJI
+            }
+            "angry" -> {
+                tvEmoji.text = ANGRY_EMOJI
+            }
+            "anxious" -> {
+                tvEmoji.text = NEUTRAL_EMOJI
+            }
+            "crying" -> {
+                tvEmoji.text = CRYING_EMOJI
+            }
+            "scared" -> {
+                tvEmoji.text = SCARED_EMOJI
+            }
+            "tired" -> {
+                tvEmoji.text = TIRED_EMOJI
+            }
+            "meh" -> {
+                tvEmoji.text = IDK_EMOJI
+            }
+            else -> {
+                tvEmoji.text = IDK_EMOJI
+            }
+        }
+    }
+
+    private fun keywordsDefinition() {
+        happyKeywords = arrayListOf(
+            "happy", "smiling", "pleased", "cheerful" , "merry", "delighted", "peaceful", "elated",
+            "lively", "pleasant", "ecstatic", "content", "grateful", "radiant", "amused"
+        )
+
+        sadKeywords = arrayListOf(
+            "sad", "sorry", "unhappy", "mournful", "heartbroken", "cheerless", "grief", "hurting", "hurt",
+            "gloomy", "dull", "low", "troubled", "sick at heart", "not happy", "down", "overwhelmed", "upset",
+            "blue", "miserable", "distressed", "heavy hearted", "tearful", "frustrated"
+        )
+
+        angryKeywords = arrayListOf(
+            "annoyed", "angry", "offended", "bitter", "enraged", "furious", "heated", "irritated",
+            "resentful", "mad", "uptight", "irritable", "irritating", "pissed", "provoked", "sulking",
+            "raging", "fuming", " hateful"
+        )
+
+        depressedKeywords = arrayListOf(
+            "depressed", "hopeless", "numb", "isolated", "alone", "joyless", "defeated", "discouraged",
+            "disheartened", "lifeless", "empty", "null", "nothing", "despairing"
+        )
+
+        anxiousKeywords = arrayListOf(
+            "anxious", "nervous", "anxiety", "worried", "uneasy", "tense", "restless", "stressed", "panicky",
+            "panick", "unsettled", "insecure"
+        )
+
+        scaredKeywords = arrayListOf(
+            "fearful", "scared", "terrifed", "horror", "frightened", "startled", "shaken", "horror stricken"
+        )
+
+        tiredKeywords = arrayListOf(
+            "tired", "sleepy", "fatigued", "exhausted", "drained", "weary", "lethargic", "worn out",
+            "ready to drop", "burnt out", "heavy eyed"
+        )
+
+        cryingKeywords = arrayListOf(
+            "crying", "weeping", "sobbing", "wailing", "bawling", "emotional", "pained", "agony", "misery",
+            "woe"
+        )
+        mehKeywords = arrayListOf(
+            "meh", "blah", "don't know", "bored", "unimpressed"
+        )
+
+        relievedKeywords = arrayListOf(
+            "relieved", "calm", "thankful", "released", "eased", "soothed", "glad"
+        )
     }
 }
--- app/src/main/java/com/example/mindmate/utilities/constants.kt
+++ app/src/main/java/com/example/mindmate/utilities/constants.kt
@@ -1,3 +1,14 @@
 package com.example.mindmate.utilities
 
 const val DATABASE_NAME = "journal_database"
+const val HAPPY_EMOJI = "\ud83d\ude03"
+const val SAD_EMOJI =  "\ud83d\ude1f"
+const val DEPRESSED_EMOJI = "\ud83d\ude14"
+const val ANGRY_EMOJI = "\ud83d\ude20"
+const val CRYING_EMOJI = "\ud83d\ude2d"
+const val SCARED_EMOJI = 	"\ud83d\ude30"
+const val NEUTRAL_EMOJI =  "\ud83d\ude10"
+const val TIRED_EMOJI =  "\ud83d\ude29"
+const val RELEIVED_EMOJI =  "\ud83d\ude0c"
+const val IDK_EMOJI =  "\ud83e\udd10"
+
